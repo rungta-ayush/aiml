65ba88b9ee4d8dcde91fb5ee314dc8ed23a10ef8
Ayush Rungta <54234619+rungta-ayush@users.noreply.github.com>
2023-01-03 23:39:29 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,43 +1,53 @@
-from sklearn.cluster import KMeans
-from sklearn.mixture import GaussianMixture
-import sklearn.metrics as metrics
+import matplotlib.pyplot as plt 
+from sklearn import datasets 
+from sklearn.cluster import KMeans 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width', 'Class']
+# import some data to play with 
+iris = datasets.load_iris()
+X = pd.DataFrame(iris.data)
+X.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width'] 
+y = pd.DataFrame(iris.target)
+y.columns = ['Targets']
 
-dataset = pd.read_csv("8.csv", names=names)
+# Build the K Means Model
+model = KMeans(n_clusters=3)
+model.fit(X) # model.labels_ : Gives cluster no for which samples belongs to
 
-X = dataset.iloc[:, :-1]  
-
-label = {'Iris-setosa': 0,'Iris-versicolor': 1, 'Iris-virginica': 2} 
-
-y = [label[c] for c in dataset.iloc[:, -1]]
-
+# # Visualise the clustering results
 plt.figure(figsize=(14,7))
-colormap=np.array(['red','lime','black'])
+colormap = np.array(['red', 'lime', 'black'])
 
-# REAL PLOT
-plt.subplot(1,3,1)
-plt.title('Real')
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[y])
+# Plot the Original Classifications using Petal features
+plt.subplot(1, 3, 1)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y.Targets], s=40) 
+plt.title('Real Clusters')
+plt.xlabel('Petal Length') 
+plt.ylabel('Petal Width')
 
-# K-PLOT
-model=KMeans(n_clusters=3, random_state=0).fit(X)
-plt.subplot(1,3,2)
-plt.title('KMeans')
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[model.labels_])
+# Plot the Models Classifications
+plt.subplot(1, 3, 2)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[model.labels_], s=40) 
+plt.title('K-Means Clustering')
+plt.xlabel('Petal Length') 
+plt.ylabel('Petal Width')
 
-print('The accuracy score of K-Mean: ',metrics.accuracy_score(y, model.labels_))
-print('The Confusion matrixof K-Mean:\n',metrics.confusion_matrix(y, model.labels_))
+# General EM for GMM
+from sklearn import preprocessing
 
-# GMM PLOT
-gmm=GaussianMixture(n_components=3, random_state=0).fit(X)
-y_cluster_gmm=gmm.predict(X)
-plt.subplot(1,3,3)
-plt.title('GMM Classification')
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[y_cluster_gmm])
+# transform your data such that its distribution will have a # mean value 0 and standard deviation of 1.
+scaler = preprocessing.StandardScaler() 
+scaler.fit(X)
+xsa = scaler.transform(X)
+xs = pd.DataFrame(xsa, columns = X.columns)
+from sklearn.mixture import GaussianMixture 
+gmm = GaussianMixture(n_components=40) 
+gmm.fit(xs)
+plt.subplot(1, 3, 3)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[0], s=40) 
+plt.title('GMM Clustering')
+plt.xlabel('Petal Length') 
+plt.ylabel('Petal Width')
 
-print('The accuracy score of EM: ',metrics.accuracy_score(y, y_cluster_gmm))
-print('The Confusion matrix of EM:\n ',metrics.confusion_matrix(y, y_cluster_gmm))
+print('Observation: The GMM using EM algorithm based clustering matched the true labels more closely than the Kmeans.')
